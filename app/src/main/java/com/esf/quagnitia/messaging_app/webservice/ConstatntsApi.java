9deaa74a121dea51cef6970e059913088d27ac0c
Nikita Kurliye <nikita.kurliye@quagnitia.com>
2019-08-01 12:28:01 +0530

    urls

--- app/src/main/java/com/esf/quagnitia/messaging_app/webservice/ConstatntsApi.java
+++ app/src/main/java/com/esf/quagnitia/messaging_app/webservice/ConstatntsApi.java
@@ -5,8 +5,21 @@ package com.esf.quagnitia.messaging_app.webservice;
  */
 
 public class ConstatntsApi {
-//    static final String BASE_URL = "http://rapidxx.com/aqi/api/public/v1/";//prod
-    static final String BASE_URL = "http://rapidxx.com/aqi/api/public/test/";//test
+//    static final String BASE_URL = "http://rapidxx.com/aqi/api/public/v1/";//old
+//    static final String BASE_URL = "http://rapidxx.com/aqi/api/public/test/";//old
+
+    //after new mail from sanket
+//    TEST APIs
+//    Version 1
+//static final String BASE_URL =  "http://rapidxx.com/aqi/api/public/v1/";
+//    Version 2
+//static final String BASE_URL =  "http://rapidxx.com/aqi/api/public/v2/"; - Here we are changing test to v2
+
+//    PROD APIs
+//    Version 1
+//static final String BASE_URL = "https://aqi.esf.edu.hk/api/public/v1/";
+//    Version 2
+static final String BASE_URL = "https://aqi.esf.edu.hk/api/public/v2/";
 
     static final String LOGIN_URL = "login";
     static final String FORGOT_URL="forgotpassword";
